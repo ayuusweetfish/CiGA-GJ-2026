4a462b538022efb3eea260c076e47fd7814ec5b3
Ayu <claire@ayu.land>
2026-07-03 02:43:23 +0800

    Empty game & more build scaffolding
    Blow-Something@8a7928e5f

---
+++ misc/build.sh
@@ -0,0 +1,27 @@
+LOVE_FILE="/tmp/CGJ2026_🐟🐐🍎🌰/CGJ2026_🐟🐐🍎🌰.love"
+
+t=$(mktemp -d)
+wd=$PWD
+cp -pr img main.lua src "$t/"
+(
+  cd "$t/" || exit
+  if [ "$1" = "min" ]; then
+    find . -type f -name '*.lua' | while read i; do
+      node "$wd/misc/luamin-env/node_modules/luamin/bin/luamin" -f "$i" > "$t/_tmp"
+      mv "$t/_tmp" "$i"
+    done
+  fi
+  find . -exec touch -t 198001010000 {} +
+  rm -f ${LOVE_FILE}
+  mkdir -p "$(dirname "${LOVE_FILE}")"
+  find . -type f -print | sort | zip ${LOVE_FILE} -X -@ -9
+  sha1sum ${LOVE_FILE}
+)
+
+if [ -n "$USE_BOON" ]; then
+  BOON=${BOON:-/mnt/data/tools/boon/target/release/boon}
+  cp misc/Boon.toml "$t/"
+  (cd "$t"; ${BOON} build . --target all)
+fi
+
+rm -rf "$t"
