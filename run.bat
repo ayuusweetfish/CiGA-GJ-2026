adc146427a9a9a39493aee9a3167da7114c73eb3
Ayu <claire@ayu.land>
2026-07-03 04:22:08 +0800

    Actually portable batch script?!

--- run.bat
+++ run.bat
@@ -1,1 +1,12 @@
-start .\love-11.5-win64\love.exe .
+:<<BATCH
+  start .\misc\love-11.5-win64\love.exe .
+  exit /b
+BATCH
+
+wd=$(dirname "$0")
+
+if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" = "darwin" ]; then
+  "$wd/misc/love.app/Contents/MacOS/love" "$wd"
+else
+  love "$wd"
+fi
