08fad7bad6c7593e1d5e2622a5f6c155cd800846
Ayu <claire@ayu.land>
2026-07-04 18:07:36 +0800

    Better automatic runs

--- misc/datasets/oid/chain_montage/run.sh
+++ misc/datasets/oid/chain_montage/run.sh
@@ -1,2 +1,4 @@
-for i in {1..7}; do python chain.py list_${i}.txt --gap 0 --output montage_${i}.png; done
-montage montage_*.png -tile 1x -geometry +0+20 montage_all.png
+rm /tmp/oid_montage_*.png
+for i in list_*.txt; do python chain.py ${i} --gap 0 --output /tmp/oid_montage_${i}.png; done
+montage /tmp/oid_montage_*.png -tile 1x -geometry +0+20 montage_all.png
+rm /tmp/oid_montage_*.png
